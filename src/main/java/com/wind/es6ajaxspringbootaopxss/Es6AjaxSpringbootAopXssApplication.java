7ca5a0008bb5ee1e5ee1df0d78c3e16ff6d6d9d9
lee-wind <2088039037@qq.com>
2021-05-25 18:21:39 +0800

    zip4j

--- src/main/java/com/wind/es6ajaxspringbootaopxss/Es6AjaxSpringbootAopXssApplication.java
+++ src/main/java/com/wind/es6ajaxspringbootaopxss/Es6AjaxSpringbootAopXssApplication.java
@@ -1,11 +1,17 @@
 package com.wind.es6ajaxspringbootaopxss;
 
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.model.enums.AesKeyStrength;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
 
 @SpringBootApplication
 public class Es6AjaxSpringbootAopXssApplication {
@@ -23,6 +29,31 @@ public class Es6AjaxSpringbootAopXssApplication {
 
         MultipartConfigFactory factory = new MultipartConfigFactory();
         factory.setMaxFileSize("1000MB");
+
+        try {
+            compressFile();
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+
         return factory.createMultipartConfig();
+    }
+
+    private void compressFile() throws ZipException {
+
+        //String classPath = getClass().getResource("/").getPath().replaceFirst("/", "");
+
+        /*for(String filename: new File(path).list()){
+            System.out.println(filename);
+        }
+        return;*/
+
+        ZipParameters zipParameters = new ZipParameters();
+        zipParameters.setEncryptFiles(true);
+        zipParameters.setEncryptionMethod(EncryptionMethod.AES);
+        zipParameters.setAesKeyStrength(AesKeyStrength.KEY_STRENGTH_256);
+
+        ZipFile zipFile = new ZipFile("webapp1.zip", "wind".toCharArray());
+        zipFile.addFolder(new File("D://work//ES6-Ajax-SpringBoot-Aop-XSS//src//main//webapp//webapp1"), zipParameters);
     }
 }
